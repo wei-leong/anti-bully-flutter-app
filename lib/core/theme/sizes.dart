cca02a628e900dcec99ac7e2706f8d91ef5331a2
Hoe Wei Leong <weileonghoe@gmail.com>
2025-12-20 10:54:38 +0800

    chore(core): add global spacing and padding constants

---
+++ lib/core/theme/sizes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const double kSmallPadding = 8;
+const double kDefaultPadding = 16;
+const double kDefaultRadius = 16;
+const double kMainIconSize = 75;
+
+const kGap0 = SizedBox(height: 0, width: 0);
+const kGap8 = SizedBox(height: 8, width: 8);
+const kGap16 = SizedBox(height: 16, width: 16);
