0ce83dc6b54d2b5fec8b3031635efca37c65a9b9
Hoe Wei Leong <weileonghoe@gmail.com>
2026-02-04 16:55:29 +0800

    feat: finished Resource Screen

--- lib/features/users/resources/presentation/screens/resource_screen.dart
+++ lib/features/users/resources/presentation/screens/resource_screen.dart
@@ -12,8 +12,8 @@ class ResourceScreen extends StatefulWidget {
 }
 
 class _ResourceScreenState extends State<ResourceScreen> {
-  String _selectedFilters = "All";
-  final List<String> _filters = ["All", "Articles", "Videos", "News"];
+  String _selectedFilters = "Articles";
+  final List<String> _filters = ["Articles", "Videos", "News", "Events"];
 
   // Mock Data
   // TODO : Remove after finishing Part 1
@@ -21,25 +21,26 @@ class _ResourceScreenState extends State<ResourceScreen> {
     ResourceItem(
       title: "How to Prevent Bullying",
       source: "UNICEF",
-      type: "article",
+      type: "articles",
       durationOrSize: "5 min",
+      imageUrl: 'https://img.freepik.com/free-psd/world-day-bullying-prevention-template-design_23-2151371506.jpg?semt=ais_hybrid&w=740&q=80'
     ),
     ResourceItem(
       title: "What is Cyberbullying?",
       source: "StopBullying.gov",
-      type: "article",
+      type: "articles",
       durationOrSize: "3 min",
     ),
     ResourceItem(
       title: "Be a Buddy, Not a Bully",
       source: "Edu Series",
-      type: "video",
+      type: "videos",
       durationOrSize: "03:45",
     ),
     ResourceItem(
       title: "The Price of Shame",
       source: "TedTalk",
-      type: "video",
+      type: "videos",
       durationOrSize: "22:10",
     ),
   ];
@@ -47,24 +48,15 @@ class _ResourceScreenState extends State<ResourceScreen> {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    List<ResourceItem> displayedItems = _allResources;
+    List<ResourceItem> displayedItems;
 
-    void returnResource() {
-      if (_selectedFilters == "All") {
-        displayedItems = _allResources;
-      } else {
-        displayedItems = _allResources
-            .where(
-              (item) =>
-                  item.type.toLowerCase() ==
-                  _selectedFilters.toLowerCase().substring(
-                    0,
-                    item.type.length > 5 ? 5 : 4,
-                  ),
-            )
-            .toList();
-      }
-    }
+    displayedItems = _allResources
+        .where(
+          (item) =>
+              item.type.toLowerCase() ==
+              _selectedFilters.toLowerCase(),
+        )
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -88,37 +80,39 @@ class _ResourceScreenState extends State<ResourceScreen> {
             // ),
             // Filter Chips
             // Single Child Scroll View
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: _filters.map((item) {
-                  final isSelected = _selectedFilters == item;
-                  return Padding(
-                    padding: EdgeInsets.only(right: kDefaultPadding),
-                    child: FilterChip(
-                      label: Text(item),
-                      selected: isSelected,
-                      onSelected: (bool selected) {
-                        setState(() {
-                          _selectedFilters = item;
-                          returnResource();
-                        });
-                      },
-                      showCheckmark: false,
-                    ),
-                  );
-                }).toList(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: kDefaultPadding),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: _filters.map((item) {
+                    final isSelected = _selectedFilters == item;
+                    return Padding(
+                      padding: EdgeInsets.only(right: kDefaultPadding),
+                      child: FilterChip(
+                        label: Text(item),
+                        selected: isSelected,
+                        onSelected: (bool selected) {
+                          setState(() {
+                            _selectedFilters = item;
+                          });
+                        },
+                        showCheckmark: false,
+                      ),
+                    );
+                  }).toList(),
+                ),
               ),
             ),
             // Content Area
             Expanded(
               child: ListView.builder(
-                padding: const EdgeInsets.all(kDefaultPadding),
+                // padding: const EdgeInsets.all(kDefaultPadding),
                 itemCount: displayedItems.length,
                 itemBuilder: (context, index) {
                   final item = displayedItems[index];
                   // Choose widget based on type
-                  if (item.type == "video") {
+                  if (item.type == "videos") {
                     return Padding(
                       padding: const EdgeInsets.only(bottom: 16.0),
                       child: VideoCard(resourceItem: item),
