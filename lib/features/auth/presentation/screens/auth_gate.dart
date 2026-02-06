ab7397e29e3886ccfb26a249bd3bc35e343a45b5
Hoe Wei Leong <weileonghoe@gmail.com>
2026-02-06 16:42:47 +0800

    feat: add AuthGate for future authentication use

---
+++ lib/features/auth/presentation/screens/auth_gate.dart
@@ -0,0 +1,27 @@
+import 'package:apu_assignment/core/navigation/main_wrapper_user.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // Loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+
+        if (snapshot.hasData) {
+          return const MainWrapperUser();
+        } else {
+          return SignInScreen(providers: [EmailAuthProvider()]);
+        }
+      },
+    );
+  }
+}
