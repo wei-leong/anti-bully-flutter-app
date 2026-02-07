b1f7de647dbd6455b2dd44e9539df7bc6700e07c
Hoe Wei Leong <weileonghoe@gmail.com>
2026-02-07 10:19:33 +0800

    feat: add bottom navigation bar wrapper for admin role

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apu_assignment/core/navigation/main_wrapper_admin.dart';
 import 'package:apu_assignment/core/navigation/main_wrapper_user.dart';
 import 'package:apu_assignment/core/theme/theme_black_white.dart';
 import 'package:apu_assignment/core/theme/util.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: theme.light(),
       darkTheme: theme.dark(),
       themeMode: ThemeMode.system,
-      home: MainWrapperUser(),
+      home: MainWrapperAdmin(),
     );
   }
 }
