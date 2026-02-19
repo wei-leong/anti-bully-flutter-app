d6899a6115638213d25082ff8be4d37d0355f63c
Hoe Wei Leong <weileonghoe@gmail.com>
2026-02-19 11:35:47 +0800

    refactor: add user_services.dart for SOC with following strick MVVM architecture

--- lib/features/auth/model/user_repository.dart
+++ lib/features/auth/model/user_repository.dart
@@ -1,30 +1,31 @@
 import 'package:apu_assignment/features/auth/model/user_model.dart';
+import 'package:apu_assignment/features/auth/model/user_services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserRepository {
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  UserServices userServices = UserServices();
 
   Future<void> saveUserData(UserModel user) async {
     try{
-      await _db.collection('user').doc(user.uid).set(user.toMap());
+      await userServices.saveUserData(user.uid, user.toMap());
     } catch (e){
       throw Exception('Failed to Save User Data: $e');
     }
   }
 
-  Future<bool> isUserExist(String uid) async {
-    DocumentSnapshot doc = await _db.collection('user').doc(uid).get();
-    return doc.exists;
+  Future<bool> isUserExist(String uid) {
+    return userServices.checkUserExist(uid);
   }
 
   Future<UserModel?> getUser(String uid) async {
     try{
-      DocumentSnapshot doc = await _db.collection('user').doc(uid).get();
+      DocumentSnapshot doc = await userServices.getUserData(uid);
       if(!doc.exists) return null;
 
       return UserModel.fromMap(doc.data() as Map<String,dynamic>, uid);
     } catch (e){
-      print("Error fetching user : $e");
+      // print("Error fetching user : $e");
       return null;
     }
   }
