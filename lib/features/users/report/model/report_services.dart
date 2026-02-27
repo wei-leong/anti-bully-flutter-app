e8b5fee08c9ce43d7e9b80e0a10a69e7d5eeea29
Hoe Wei Leong <weileonghoe@gmail.com>
2026-02-27 12:33:48 +0800

    feat: add code to report_services and report_repository to get reports made by user

--- lib/features/users/report/model/report_services.dart
+++ lib/features/users/report/model/report_services.dart
@@ -3,7 +3,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ReportServices {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  Future<void> saveReport(Map<String,dynamic> rawData) async {
+  Future<void> saveReport(Map<String, dynamic> rawData) async {
     await _db.collection('reports').add(rawData);
+  }
+
+  Future<QuerySnapshot<Map<String, dynamic>>> getUserReports(String uid) async {
+    return await _db
+        .collection('reports')
+        .where('uid', isEqualTo: uid)
+        .orderBy('createdAt', descending: true)
+        .get();
   }
 }
