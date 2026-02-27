a8c119729edbe66fbb8a0aff19225c68fe45fca9
Hoe Wei Leong <weileonghoe@gmail.com>
2026-02-27 13:18:24 +0800

    feat: add providers to get report data

--- lib/features/users/report/data/report_providers.dart
+++ lib/features/users/report/data/report_providers.dart
@@ -1,3 +1,5 @@
+import 'package:apu_assignment/features/auth/data/auth_providers.dart';
+import 'package:apu_assignment/features/users/report/model/report_model.dart';
 import 'package:apu_assignment/features/users/report/model/report_repository.dart';
 import 'package:apu_assignment/features/users/report/model/report_services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,4 +11,16 @@ final reportServicesProvider = Provider<ReportServices>((ref) {
 final reportRepositoryProvider = Provider<ReportRepository>((ref) {
   final services = ref.watch(reportServicesProvider);
   return ReportRepository(services);
+});
+
+final userReportsProvider = FutureProvider.autoDispose<List<ReportModel>>((ref) async {
+  final auth = ref.watch(firebaseAuthProvider);
+  final currentUser = auth.currentUser;
+
+  if(currentUser == null){
+    return [];
+  }
+
+  final repository = ref.watch(reportRepositoryProvider);
+  return await repository.getUserReports(currentUser.uid);
 });
