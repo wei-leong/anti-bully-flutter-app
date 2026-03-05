722b65e1483b6e871ca3f79e9d2ec66c51068e14
Hoe Wei Leong <weileonghoe@gmail.com>
2026-03-05 12:29:04 +0800

    feat: modify providers and repository to add getChatRooms from chat service

--- lib/features/chat/data/chat_providers.dart
+++ lib/features/chat/data/chat_providers.dart
@@ -1,6 +1,7 @@
 import 'package:apu_assignment/features/auth/data/auth_providers.dart';
 import 'package:apu_assignment/features/chat/domain/chat_model.dart';
 import 'package:apu_assignment/features/chat/domain/chat_repository.dart';
+import 'package:apu_assignment/features/chat/domain/chat_room_model.dart';
 import 'package:apu_assignment/features/chat/domain/chat_services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -24,4 +25,9 @@ final chatStreamProvider = StreamProvider.autoDispose.family<List<ChatModel>, St
   final repository = ref.watch(chatRepositoryProvider);
 
   return repository.getMessages(currentUser.uid, receiverUid);
+});
+
+final chatRoomsStreamProvider = StreamProvider.autoDispose.family<List<ChatRoomModel>,String>((ref, uid) {
+  final repository = ref.watch(chatRepositoryProvider);
+  return repository.getChatRooms(uid);
 });
--- lib/features/chat/domain/chat_repository.dart
+++ lib/features/chat/domain/chat_repository.dart
@@ -1,4 +1,5 @@
 import 'package:apu_assignment/features/chat/domain/chat_model.dart';
+import 'package:apu_assignment/features/chat/domain/chat_room_model.dart';
 import 'package:apu_assignment/features/chat/domain/chat_services.dart';
 
 class ChatRepository {
@@ -6,11 +7,13 @@ class ChatRepository {
 
   ChatRepository(this.chatServices);
 
-  Future<void> sendMessage(
-    ChatModel message
-  ) async {
+  Future<void> sendMessage(ChatModel message) async {
     try {
-      await chatServices.sendMessage(message.toMap(), message.senderUid, message.receiverUid);
+      await chatServices.sendMessage(
+        message.toMap(),
+        message.senderUid,
+        message.receiverUid,
+      );
     } catch (e) {
       throw Exception('Failed to Send Message: $e');
     }
@@ -19,13 +22,26 @@ class ChatRepository {
   Stream<List<ChatModel>> getMessages(String senderUid, String receiverUid) {
     try {
       final rawStream = chatServices.getMessages(senderUid, receiverUid);
-      return rawStream.map((snapshot){
-        return snapshot.docs.map((doc){
+      return rawStream.map((snapshot) {
+        return snapshot.docs.map((doc) {
           return ChatModel.fromMap(doc.data(), doc.id);
         }).toList();
       });
     } catch (e) {
       throw Exception('Failed to Receive Message: $e');
+    }
+  }
+
+  Stream<List<ChatRoomModel>> getChatRooms(String uid) {
+    try {
+      final rawData = chatServices.getChatRooms(uid);
+      return rawData.map((snapshot) {
+        return snapshot.docs.map((doc) {
+          return ChatRoomModel.fromMap(doc.data(), doc.id);
+        }).toList();
+      });
+    } catch (e) {
+      throw Exception('Failed to get chat lists : $e');
     }
   }
 }
